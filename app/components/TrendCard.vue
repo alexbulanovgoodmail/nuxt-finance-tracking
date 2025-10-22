f7511014e01e282093c229aad2b4665fe88f0101
Александр Буланов <alexbulanovgoodmail@gmail.com>
2025-10-22 12:47:54 +0300

    Компонент карточки трендов, цвет и вывод разницы процентах

--- app/components/TrendCard.vue
+++ app/components/TrendCard.vue
@@ -1,26 +1,39 @@
 <script setup lang="ts">
 interface Props {
 	title: string
-	icon: string
 	amount: number
 	lastAmount: number
 	color: 'success' | 'error'
 	loading?: boolean
 }
 
-defineProps<Props>()
+const { amount, lastAmount } = defineProps<Props>()
+const trendingUp = computed<boolean>(() => amount >= lastAmount)
+const trendIcon = computed<string>(() =>
+	trendingUp.value ? 'i-lucide-trending-up' : 'i-lucide-trending-down'
+)
+const percentageTrend = computed<string>(() => {
+	if (amount === 0 || lastAmount === 0) return '♾️'
+
+	const bigger = Math.max(amount, lastAmount)
+	const lower = Math.min(amount, lastAmount)
+
+	const ration = ((bigger - lower) / lower) * 100
+
+	return `${Math.ceil(ration)}%`
+})
 </script>
 
 <template>
 	<UPageCard
 		variant="subtle"
 		:title="loading ? undefined : title"
-		:icon="loading ? undefined : icon"
+		:icon="loading ? undefined : trendIcon"
 		:ui="{
 			container: `gap-y-1.5 sm:${loading ? 'p-0' : ''}`,
 			wrapper: 'items-start',
-			leading: `p-2.5 rounded-full bg-${color}/10 ring ring-inset ring-${color}/25`,
-			leadingIcon: `text-${color}`,
+			leading: `p-2.5 rounded-full bg-${trendingUp ? 'success' : 'error'}/10 ring ring-inset ring-${trendingUp ? 'success' : 'error'}/25`,
+			leadingIcon: `text-${trendingUp ? 'success' : 'error'}`,
 			title: 'font-normal text-muted text-xs uppercase'
 		}"
 		class="lg:rounded-none first:rounded-l-lg last:rounded-r-lg hover:z-1"
@@ -31,11 +44,15 @@ defineProps<Props>()
 			</template>
 			<template v-else>
 				<span class="text-2xl font-semibold text-highlighted">
-					{{ amount }} $
+					{{ amount }}
 				</span>
 
-				<UBadge :color="color" variant="subtle" class="text-xs">
-					+ {{ 10 }}%
+				<UBadge
+					:color="trendingUp ? 'success' : 'error'"
+					variant="subtle"
+					class="text-xs"
+				>
+					{{ percentageTrend }}
 				</UBadge>
 			</template>
 		</div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -49,7 +49,6 @@ const period = ref<Period>('monthly')
 			<UPageGrid class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px">
 				<TrendCard
 					:title="t('transactions.income')"
-					:icon="'i-lucide-trending-up'"
 					:amount="4000"
 					:last-amount="3000"
 					color="success"
@@ -57,24 +56,21 @@ const period = ref<Period>('monthly')
 				/>
 				<TrendCard
 					:title="t('transactions.income')"
-					:icon="'i-lucide-trending-up'"
 					:amount="4000"
-					:last-amount="3000"
+					:last-amount="13000"
 					color="error"
-					:loading="true"
+					:loading="false"
 				/>
 				<TrendCard
-					:title="t('transactions.income')"
-					:icon="'i-lucide-trending-up'"
+					:title="t('transactions.investment')"
 					:amount="4000"
-					:last-amount="3000"
+					:last-amount="23000"
 					color="success"
 					:loading="false"
 				/>
 				<TrendCard
-					:title="t('transactions.income')"
-					:icon="'i-lucide-trending-up'"
-					:amount="4000"
+					:title="t('transactions.savings')"
+					:amount="14000"
 					:last-amount="3000"
 					color="error"
 					:loading="false"
